b5dd2ff1a31d6ced3aa9333c2ed9291f5eb280b9
David <davidifme@icloud.com>
2024-11-23 19:06:15 +0100

    Add functions for the basic math operators

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculatorAdd(a, b) {
+    return a + b;
+}
+
+function calculatorSubtract(a, b) {
+    return a - b;
+}
+
+function calculatorMultiply(a, b) {
+    return a * b;
+}
+
+function calculatorDivide(a, b) {
+    return a / b;
+}

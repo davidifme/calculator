b0113dbdf756848f825108d778a00fb56e65da02
David <davidifme@icloud.com>
2024-11-25 20:00:23 +0100

    refine functions structure and logic

--- script.js
+++ script.js
@@ -1,27 +1,85 @@
-function calculatorOperation(a, operator, b) {
-    const numberA = a;
-    const numberB = b;
-    const operatorAB = operator;
-    let result = 0;
+function add(a, b) {
+    return a + b;
+}
 
-    switch (operatorAB) {
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, operator, b) {
+    let result = 0;
+    const numberA = Number(a);
+    const numberB = Number(b);
+
+    switch(operator) {
         case '+':
-            result = numberA + numberB;
+            result = add(numberA, numberB);
             break;
         case '-':
-            result = numberA - numberB;
-            break;
+            result = subtract(numberA, numberB);
+           break;
         case '*':
-            result = numberA * numberB;
-            break;
+            result = multiply(numberA, numberB);
+           break;
         case '/':
-            result = numberA * numberB;
-            break;   
-
+            result = divide(numberA, numberB);
+          break;
         default:
-            console.log('ERROR! Invalid operator.')
+            console.log('Invalid operator.')
             break;
     }
 
     return result;
 }
+
+const numericButtons = document.querySelectorAll('.number-button');
+const operationButtons = document.querySelectorAll('.operation-button');
+const display = document.querySelector('.calculator-display');
+
+let displayValue = '';
+let firstNumber = 0;
+let secondNumber = 0;
+let currentOperator = '';
+
+numericButtons.forEach((number) => {
+    number.addEventListener(("click"), () => {
+        const buttonValue = number.textContent;
+
+        if (display.textContent.length <= 12) {
+            if (display.textContent == 0 || display.textContent === '') {
+                display.textContent = buttonValue;
+                displayValue = buttonValue;
+            } else {
+                display.textContent += buttonValue;
+                displayValue += buttonValue;
+            }
+        }
+    });
+});
+
+operationButtons.forEach((operator) => {
+    operator.addEventListener(("click"), () => {
+        if (operator.textContent === 'C') {
+            display.textContent = 0;
+            displayValue = 0;
+        } else if (operator.textContent === '=') {
+            secondNumber = display.textContent;
+            display.textContent = operate(firstNumber, currentOperator, secondNumber);
+            firstNumber = 0;
+            secondNumber = 0;
+            currentOperator = '';
+        } else {
+            firstNumber = display.textContent;
+            display.textContent = '';
+            currentOperator = operator.textContent;
+        }
+    });
+});

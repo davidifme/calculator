c3e5a93b39c29e5b751813cb7ad912928f6327fa
David <davidifme@icloud.com>
2024-11-26 13:15:59 +0100

    fix calculator funcionality
    Final state of the application (v1.0)

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function operate(a, operator, b) {
             break;
     }
 
-    return result;
+    return parseFloat(result.toFixed(11));
 }
 
 const numericButtons = document.querySelectorAll('.number-button');
@@ -48,15 +48,19 @@ let displayValue = '';
 let firstNumber = 0;
 let secondNumber = 0;
 let currentOperator = '';
+let currentResult = 0;
+let typeToggle = true;
+let numberSwitch = false;
 
 numericButtons.forEach((number) => {
     number.addEventListener(("click"), () => {
         const buttonValue = number.textContent;
 
         if (display.textContent.length <= 12) {
-            if (display.textContent == 0 || display.textContent === '') {
+            if (typeToggle === false || display.textContent == 0 || display.textContent === '') {
                 display.textContent = buttonValue;
                 displayValue = buttonValue;
+                typeToggle = true;
             } else {
                 display.textContent += buttonValue;
                 displayValue += buttonValue;
@@ -70,16 +74,30 @@ operationButtons.forEach((operator) => {
         if (operator.textContent === 'C') {
             display.textContent = 0;
             displayValue = 0;
+            numberSwitch = false;
         } else if (operator.textContent === '=') {
             secondNumber = display.textContent;
-            display.textContent = operate(firstNumber, currentOperator, secondNumber);
+            currentResult = operate(firstNumber, currentOperator, secondNumber);
+            display.textContent = currentResult;
             firstNumber = 0;
             secondNumber = 0;
             currentOperator = '';
+            typeToggle = false;
+            numberSwitch = false;
         } else {
-            firstNumber = display.textContent;
-            display.textContent = '';
-            currentOperator = operator.textContent;
+            if (!numberSwitch) {
+                firstNumber = display.textContent;
+                currentOperator = operator.textContent;
+                numberSwitch = true;
+                typeToggle = false;
+            } else {
+                secondNumber = display.textContent;
+                currentResult = operate(firstNumber, currentOperator, secondNumber);
+                currentOperator = operator.textContent;
+                firstNumber = currentResult;
+                display.textContent = currentResult;
+                typeToggle = false;
+            }
         }
     });
 });
